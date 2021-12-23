f4fe843177fea6fc30334cc351d2fe392f6eaeda
szeanx <sean@relampagos.org>
2021-12-23 13:21:07 +0800

    added components and chat page

---
+++ components/Sidebar.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h1>This is sidebar.</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
+
+const Container = styled.div``;
